b555278a1f6c713f1f29b7bb24f00e2d3f8b6411
Narayut Putjaika <narayutput@gmail.com>
2019-06-27 16:35:28 +0700

    save

--- resources/js/complaint/views/Show.vue
+++ resources/js/complaint/views/Show.vue
@@ -1,34 +1,34 @@
 <template>
-  <v-layout row wrap>
-    <v-flex xs12>
-      <custom-toolbar id="showComplaintToolbar" v-if="activeComplaint" :items="items"></custom-toolbar>
-
-      <v-layout align-center justify-center class="loading__wrapper">
+    <v-layout row wrap>
         <v-flex xs12>
-          <transition name="slide-y-reverse-transition" appear>
-            <v-progress-circular
-              v-if="$_complaint_mixin_isFetchingShowComplaint || isUpdatingStatus"
-              :size="70"
-              :width="7"
-              color="deep-orange"
-              indeterminate
-            ></v-progress-circular>
-          </transition>
-        </v-flex>
-      </v-layout>
+            <custom-toolbar id="showComplaintToolbar" v-if="activeComplaint" :items="items"></custom-toolbar>
 
-      <v-layout class="content__wrapper">
-        <v-flex xs12>
-          <transition name="slide-y-reverse-transition" appear>
-            <complaint-detail-card
-              v-if="!$_complaint_mixin_isFetchingShowComplaint"
-              :issue-id="$route.params.issue"
-              class="pl-5 pr-4"
-            />
-          </transition>
-        </v-flex>
-      </v-layout>
-      <!-- <v-tabs-items v-model="tab">
+            <v-layout align-center justify-center class="loading__wrapper">
+                <v-flex xs12>
+                    <transition name="slide-y-reverse-transition" appear>
+                        <v-progress-circular
+                            v-if="$_complaint_mixin_isFetchingShowComplaint || isUpdatingStatus"
+                            :size="70"
+                            :width="7"
+                            color="deep-orange"
+                            indeterminate
+                        ></v-progress-circular>
+                    </transition>
+                </v-flex>
+            </v-layout>
+
+            <v-layout class="content__wrapper">
+                <v-flex xs12>
+                    <transition name="slide-y-reverse-transition" appear>
+                        <complaint-detail-card
+                            v-if="!$_complaint_mixin_isFetchingShowComplaint"
+                            :issue-id="$route.params.issue"
+                            class="pl-5 pr-4"
+                        />
+                    </transition>
+                </v-flex>
+            </v-layout>
+            <!-- <v-tabs-items v-model="tab">
         <v-tab-item
           v-for="(tab, tabIndex) in tabs"
           :key="`tabItem-${tabIndex}`"
@@ -36,165 +36,165 @@
         >
           <complaint-list :type="tab.type" :active="isTabActive(`tab-${tabIndex}`)"></complaint-list>
         </v-tab-item>
-      </v-tabs-items>-->
-    </v-flex>
+            </v-tabs-items>-->
+        </v-flex>
 
-    <complaint-form
-      v-if="complaintDialog"
-      :managable-module="vuex.modules.COMPLAINT"
-      :managable-route-param="complaintRouteParam"
-      :managable-edit="hasActiveComplaint"
-    />
+        <complaint-form
+            v-if="complaintDialog"
+            :managable-module="vuex.modules.COMPLAINT"
+            :managable-route-param="complaintRouteParam"
+            :managable-edit="hasActiveComplaint"
+        />
 
-    <message-alert
-      key="alertComplaintShow"
-      :alertable-visible.sync="alertable_alert"
-      :alertable-type="alertable_type"
-      :alertable-messages="alertable_messages"
-      :alertable-props="alertable_props"
-    ></message-alert>
-  </v-layout>
+        <message-alert
+            key="alertComplaintShow"
+            :alertable-visible.sync="alertable_alert"
+            :alertable-type="alertable_type"
+            :alertable-messages="alertable_messages"
+            :alertable-props="alertable_props"
+        ></message-alert>
+    </v-layout>
 </template>
 
 <script>
-import { vuex } from "../../mixins/vuexable";
-import alertable from "../../mixins/alertable";
-import CustomToolbar from "../../components/CustomToolbar";
-import MessageAlert from "../../components/MessageAlert";
-import ComplaintForm from "../components/ComplaintForm";
-import complaintMixin from "../../mixins/complaint-mixin";
-import ComplaintStatus from "../components/ComplaintStatus";
-import ComplaintDetailCard from "../components/ComplaintDetailCard";
+    import { vuex } from "../../mixins/vuexable";
+    import alertable from "../../mixins/alertable";
+    import CustomToolbar from "../../components/CustomToolbar";
+    import MessageAlert from "../../components/MessageAlert";
+    import ComplaintForm from "../components/ComplaintForm";
+    import complaintMixin from "../../mixins/complaint-mixin";
+    import ComplaintStatus from "../components/ComplaintStatus";
+    import ComplaintDetailCard from "../components/ComplaintDetailCard";
 
-export default {
-  mixins: [alertable, complaintMixin],
+    export default {
+        mixins: [alertable, complaintMixin],
 
-  components: {
-    CustomToolbar,
-    MessageAlert,
-    ComplaintForm,
-    ComplaintDetailCard
-  },
-
-  data() {
-    return {
-      alertable_messages: {
-        update_status_success: {
-          text: "Complaint Status was updated successfully.",
-          type: "success"
-        }
-      }
-    };
-  },
-
-  computed: {
-    ...vuex.mapWaitingGetters({
-      isUpdatingStatus: "updating complaint status"
-    }),
-
-    items() {
-      return [
-        {
-          icon: "arrow_back",
-          text: "Back to Inbox",
-          onClick: () => {
-            this.$router.go(-1);
-          }
+        components: {
+            CustomToolbar,
+            MessageAlert,
+            ComplaintForm,
+            ComplaintDetailCard
         },
-        {
-          icon: "archive",
-          text: "Archive",
-          onClick: async () => {
-            // archive complaint
 
-            // go back
-            this.$router.go(-1);
-          }
-        },
-        {
-          icon: "delete",
-          text: "Delete",
-          onClick: async () => {
-            // delete complaint
-
-            // go back
-            this.$router.go(-1);
-          }
-        },
-        { divider: true },
-        {
-          menu: true,
-          component: () => ComplaintStatus,
-          componentProps: () => {
+        data() {
             return {
-              issueId: this.activeComplaintId
+                alertable_messages: {
+                    update_status_success: {
+                        text: "Complaint Status was updated successfully.",
+                        type: "success"
+                    }
+                }
             };
-          },
-          menuItems: this.statusesItems
         },
-        { spacer: true },
-        {
-          icon: "keyboard_arrow_left",
-          text: "Newer",
-          disabled: () => this.$_paginatable_isFirstPage,
-          onClick: () => {
-            this.$_paginatable_toPrevPage();
-          }
+
+        computed: {
+            ...vuex.mapWaitingGetters({
+                isUpdatingStatus: "updating complaint status"
+            }),
+
+            items() {
+                return [
+                    {
+                        icon: "arrow_back",
+                        text: "Back to Inbox",
+                        onClick: () => {
+                            this.$router.go(-1);
+                        }
+                    },
+                    {
+                        icon: "archive",
+                        text: "Archive",
+                        onClick: async () => {
+                            // archive complaint
+
+                            // go back
+                            this.$router.go(-1);
+                        }
+                    },
+                    {
+                        icon: "delete",
+                        text: "Delete",
+                        onClick: async () => {
+                            // delete complaint
+
+                            // go back
+                            this.$router.go(-1);
+                        }
+                    },
+                    { divider: true },
+                    {
+                        menu: true,
+                        component: () => ComplaintStatus,
+                        componentProps: () => {
+                            return {
+                                issueId: this.activeComplaintId
+                            };
+                        },
+                        menuItems: this.statusesItems
+                    },
+                    { spacer: true },
+                    {
+                        icon: "keyboard_arrow_left",
+                        text: "Newer",
+                        disabled: () => this.$_paginatable_isFirstPage,
+                        onClick: () => {
+                            this.$_paginatable_toPrevPage();
+                        }
+                    },
+                    {
+                        icon: "keyboard_arrow_right",
+                        text: "Older",
+                        disabled: () => this.$_paginatable_isLastPage,
+                        onClick: () => {
+                            this.$_paginatable_toNextPage();
+                        }
+                    },
+                    { icon: "settings", text: "Settings" }
+                ];
+            },
+
+            statuses() {
+                return this.$_vuexable_getSortedValues(vuex.modules.ISSUE_STATUS);
+            },
+
+            statusesItems() {
+                return this.statuses.map(({ id, status }) => {
+                    return {
+                        text: status,
+                        disabled: () => this.activeComplaint.issue_status_id === id,
+                        onClick: async () => {
+                            await this[vuex.actions.UPDATE]({
+                                id: this.activeComplaintId,
+                                issue_status_id: id,
+                                includes: [],
+                                routeParam: {
+                                    issue: this.activeComplaintId
+                                }
+                            });
+
+                            this.$_alertable_alert("update_status_success");
+                        }
+                    };
+                });
+            }
         },
-        {
-          icon: "keyboard_arrow_right",
-          text: "Older",
-          disabled: () => this.$_paginatable_isLastPage,
-          onClick: () => {
-            this.$_paginatable_toNextPage();
-          }
-        },
-        { icon: "settings", text: "Settings" }
-      ];
-    },
 
-    statuses() {
-      return this.$_vuexable_getSortedValues(vuex.modules.ISSUE_STATUS);
-    },
-
-    statusesItems() {
-      return this.statuses.map(({ id, status }) => {
-        return {
-          text: status,
-          disabled: () => this.activeComplaint.issue_status_id === id,
-          onClick: async () => {
-            await this[vuex.actions.UPDATE]({
-              id: this.activeComplaintId,
-              issue_status_id: id,
-              includes: [],
-              routeParam: {
-                issue: this.activeComplaintId
-              }
-            });
-
-            this.$_alertable_alert("update_status_success");
-          }
-        };
-      });
-    }
-  },
-
-  methods: {
-    ...vuex.mapWaitingActions(vuex.modules.COMPLAINT, {
-      [vuex.actions.UPDATE]: "updating complaint status"
-    })
-  }
-};
+        methods: {
+            ...vuex.mapWaitingActions(vuex.modules.COMPLAINT, {
+                [vuex.actions.UPDATE]: "updating complaint status"
+            })
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
-.content {
-  &__wrapper {
-    height: calc(100% - 60px);
-    overflow: auto;
-    background: white;
-  }
-}
+    .content {
+        &__wrapper {
+            height: calc(100% - 30px);
+            overflow: auto;
+            background: white;
+        }
+    }
 </style>
 
 
--- resources/js/layouts/Default.vue
+++ resources/js/layouts/Default.vue
@@ -1,121 +1,121 @@
 <template>
-  <v-app id="meaDefault">
-    <add-complaint-btn v-if="isMobile"/>
+    <v-app id="meaDefault">
+        <add-complaint-btn v-if="isMobile"/>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      clipped
-      class="grey lighten-4"
-      app
-      :mobile-break-point="mobileBreakPoint"
-    >
-      <v-list dense class="grey lighten-4">
-        <add-complaint-btn v-if="!isMobile"/>
+        <v-navigation-drawer
+            v-model="drawer"
+            fixed
+            clipped
+            class="grey lighten-4"
+            app
+            :mobile-break-point="mobileBreakPoint"
+        >
+            <v-list dense class="grey lighten-4">
+                <add-complaint-btn v-if="!isMobile"/>
 
-        <template v-for="(item, i) in items">
-          <v-layout v-if="item.heading" :key="i" row align-center>
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
-            </v-flex>
-            <v-flex xs6 class="text-xs-right">
-              <v-btn small flat>edit</v-btn>
-            </v-flex>
-          </v-layout>
+                <template v-for="(item, i) in items">
+                    <v-layout v-if="item.heading" :key="i" row align-center>
+                        <v-flex xs6>
+                            <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+                        </v-flex>
+                        <v-flex xs6 class="text-xs-right">
+                            <v-btn small flat>edit</v-btn>
+                        </v-flex>
+                    </v-layout>
 
-          <v-divider v-else-if="item.divider" :key="i" dark class="my-3"></v-divider>
+                    <v-divider v-else-if="item.divider" :key="i" dark class="my-3"></v-divider>
 
-          <v-list-tile v-else :key="i" @click>
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title class="grey--text">{{ item.text }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
+                    <v-list-tile v-else :key="i" @click>
+                        <v-list-tile-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title class="grey--text">{{ item.text }}</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
 
-    <v-toolbar id="toolbarLayoutDefault" class="elevation-0" app absolute clipped-left>
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar id="toolbarLayoutDefault" class="elevation-0" app fixed clipped-left>
+            <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
 
-      <span class="title ml-3 mr-5">
-        MEA
-        <span class="font-weight-light">Complaint</span>
-      </span>
+            <span class="title ml-3 mr-5">
+                MEA
+                <span class="font-weight-light">Complaint</span>
+            </span>
 
-      <v-text-field
-        flat
-        solo
-        color="deep-orange"
-        hide-details
-        label="Search complaint"
-        prepend-inner-icon="search"
-      ></v-text-field>
+            <v-text-field
+                flat
+                solo
+                color="deep-orange"
+                hide-details
+                label="Search complaint"
+                prepend-inner-icon="search"
+            ></v-text-field>
 
-      <v-spacer></v-spacer>
-    </v-toolbar>
+            <v-spacer></v-spacer>
+        </v-toolbar>
 
-    <v-content>
-      <v-container fluid fill-height pa-0 class="grey lighten-4" v-resize="onResize">
-        <slot/>
-      </v-container>
-    </v-content>
-  </v-app>
+        <v-content>
+            <v-container fluid fill-height pa-0 class="grey lighten-4" v-resize="onResize">
+                <slot/>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import layoutable from "../mixins/layoutable";
-import AddComplaintBtn from "../complaint/components/AddComplaintBtn";
+    import layoutable from "../mixins/layoutable";
+    import AddComplaintBtn from "../complaint/components/AddComplaintBtn";
 
-export default {
-  mixins: [layoutable],
+    export default {
+        mixins: [layoutable],
 
-  components: {
-    AddComplaintBtn
-  },
+        components: {
+            AddComplaintBtn
+        },
 
-  data() {
-    return {
-      items: [
-        { icon: "inbox", text: "Inbox" },
-        { icon: "start", text: "Starred" },
-        { icon: "lightbulb_outline", text: "Notes" },
-        { divider: true },
-        { heading: "Labels" },
-        { icon: "add", text: "Create new label" },
-        { divider: true },
-        { icon: "send", text: "Sent" },
-        { icon: "drafts", text: "Drafts" },
-        { icon: "archive", text: "Archive" },
-        { icon: "delete", text: "Trash" },
-        { divider: true },
-        { icon: "settings", text: "Settings" }
-        //   { icon: "chat_bubble", text: "Trash" },
-        //   { icon: "help", text: "Help" },
-        //   { icon: "phonelink", text: "App downloads" },
-        //   { icon: "keyboard", text: "Keyboard shortcuts" }
-      ]
+        data() {
+            return {
+                items: [
+                    { icon: "inbox", text: "Inbox" },
+                    { icon: "start", text: "Starred" },
+                    { icon: "lightbulb_outline", text: "Notes" },
+                    { divider: true },
+                    { heading: "Labels" },
+                    { icon: "add", text: "Create new label" },
+                    { divider: true },
+                    { icon: "send", text: "Sent" },
+                    { icon: "drafts", text: "Drafts" },
+                    { icon: "archive", text: "Archive" },
+                    { icon: "delete", text: "Trash" },
+                    { divider: true },
+                    { icon: "settings", text: "Settings" }
+                    //   { icon: "chat_bubble", text: "Trash" },
+                    //   { icon: "help", text: "Help" },
+                    //   { icon: "phonelink", text: "App downloads" },
+                    //   { icon: "keyboard", text: "Keyboard shortcuts" }
+                ]
+            };
+        }
     };
-  }
-};
 </script>
 
 <style lang="stylus">
-#meaDefault {
-  .v-navigation-drawer__border {
-    display: none;
-  }
+    #meaDefault {
+        .v-navigation-drawer__border {
+            display: none;
+        }
 
-  .v-toolbar {
-    border-bottom: 1px solid #E0E0E0;
-  }
+        .v-toolbar {
+            border-bottom: 1px solid #E0E0E0;
+        }
 
-  #toolbarLayoutDefault {
-    .title + * {
-      margin-left: 4rem;
+        #toolbarLayoutDefault {
+            .title + * {
+                margin-left: 4rem;
+            }
+        }
     }
-  }
-}
 </style>
